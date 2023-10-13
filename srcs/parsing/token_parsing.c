f54cd085742cd18e31c87ff718ce4e6310cdca7c
edufour/Elodie <lalou_dufour@hotmail.com>
2023-10-13 14:09:07 -0400

    Finished parsing_pipe

--- srcs/parsing/token_parsing.c
+++ srcs/parsing/token_parsing.c
@@ -1,32 +1,58 @@
 
 #include "../../includes/minishell.h"
 
-void	parsing_pipe(char *line, int index)
+/*The below functions test the behavior of tokens surrounded by 
+all symbols required to handle in the subject. 
+Everything else is considered a string.
+If an error should occur because of an invalid string, 
+it will be handled by execve.*/
+
+//No command on left, no command on right, double pipe
+int	parsing_pipe(char *line, int index)
 {
-	if (!line[index - 1] || !line[index + 1] || line[index + 1] == '|' 
+	if (index == 0 || line[index + 1] == '|'
 		|| !ft_strncmp(&line[index], "| |", 3))
-		exit_error ("minishell: syntax error near unexpected token '|'");
+		return (print_error
+			("minishell: syntax error near unexpected token '|'"));
+	else if (!line[index + 1])
+		return (print_error
+			("minishell: syntax error near unexpected token 'newline'"));
+	return (0);
 }
 
-//<
-void	parsing_inredirect(char *line, int index)
+// < missing input file (on right), redirection symbol on right
+int	parsing_inredirect(char *line, int index)
 {
+	if (!line[index + 1])
+		return (print_error
+			("minishell: syntax error near unexpected token 'newline'"));
+	else if ()
 	
 }
 
 //>
-void	parsing_outredirect(char *line, int index)
+int	parsing_outredirect(char *line, int index)
 {
 
 }
 
 //<<
-void	parsing_heredoc(char *line, int index)
+int	parsing_heredoc(char *line, int index)
 {
 }
 
 //>>
-void	parsing_appoutredirect(char *line, int index)
+int	parsing_appoutredirect(char *line, int index)
 {
 
+}
+
+int next_isredirection(char *line, int index)
+{
+	if (ft_iswhitespace(line[index + 1] == 1))
+		if (line[index + 2] == '<' || line[index + 2] == '>' || line[index + 2] == '|')
+			return (1);
+	else
+		if (line[index + 1] == '<' || line[index + 1] == '>' || line[index + 1] == '|')
+			return (1);
 }
