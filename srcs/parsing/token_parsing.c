68dd02e4939d3b27456d2dfa18485d11f7ea7bfa
edufour/Elodie <lalou_dufour@hotmail.com>
2023-10-14 14:49:53 -0400

    Finished token parsing function, needs testing

--- srcs/parsing/token_parsing.c
+++ srcs/parsing/token_parsing.c
@@ -5,54 +5,43 @@
 all symbols required to handle in the subject. 
 Everything else is considered a string.
 If an error should occur because of an invalid string, 
-it will be handled by execve.*/
+it will be handled by execve.
+Only symbols right of the redirection is checked, as the error would 
+have been caught earlier if a redirection symbol is present on left*/
 
-//No command on left, no command on right, double pipe
-int	parsing_pipe(char *line, int index)
+int	next_isredirection(char *line, int index)
 {
-	if (index == 0 || line[index + 1] == '|'
-		|| !ft_strncmp(&line[index], "| |", 3))
-		return (print_error
-			("minishell: syntax error near unexpected token '|'"));
-	else if (!line[index + 1])
-		return (print_error
-			("minishell: syntax error near unexpected token 'newline'"));
+	if (ft_iswhitespace(line[index + 1] == 1))
+		index += 2;
+	else
+		index++;
+	if ((!double_quoted(line, index) && !single_quoted(line, index))
+		&& (line[index] == '<' || line[index] == '>' || line[index] == '|'))
+		return (1);
 	return (0);
 }
 
-// < missing input file (on right), redirection symbol on right
-int	parsing_inredirect(char *line, int index)
+int	parsing_redirection(char *line, int index)
 {
 	if (!line[index + 1])
 		return (print_error
 			("minishell: syntax error near unexpected token 'newline'"));
-	else if ()
-	
-}
-
-//>
-int	parsing_outredirect(char *line, int index)
-{
-
-}
-
-//<<
-int	parsing_heredoc(char *line, int index)
-{
-}
-
-//>>
-int	parsing_appoutredirect(char *line, int index)
-{
-
-}
-
-int next_isredirection(char *line, int index)
-{
-	if (ft_iswhitespace(line[index + 1] == 1))
-		if (line[index + 2] == '<' || line[index + 2] == '>' || line[index + 2] == '|')
-			return (1);
-	else
-		if (line[index + 1] == '<' || line[index + 1] == '>' || line[index + 1] == '|')
-			return (1);
+	if (index == 0 && line[index] == '|')
+		return (print_error
+			("minishell: syntax error near unexpected token '|'"));
+	if (next_isredirection(line, index))
+	{
+		if (ft_iswhitespace(line[index + 1]))
+			index += 2;
+		else
+			index++;
+		if (line[index + 1] && line[index] == line[index + 1])
+			printf ("minishell: syntax error near unexpected token '%c%c'",
+				line[index]);
+		else
+			printf ("minishell: syntax error near unexpected token '%c'",
+				line[index]);
+		return (ERROR);
+	}
+	return (0);
 }
