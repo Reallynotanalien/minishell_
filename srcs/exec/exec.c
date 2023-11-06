7fab6c265735e61675fd8900e83931e8b002ac95
Katherine Fortin <kafortin@student.42quebec.com>
2023-11-06 18:30:36 -0500

    K 11-06: Added the find path functions

--- srcs/exec/exec.c
+++ srcs/exec/exec.c
@@ -1,5 +1,59 @@
 # include "../../includes/minishell.h"
 
+char	*access_path(t_command *cmd, char **path_env)
+{
+	int		i;
+	char	*path_join;
+
+	i = 0;
+	while (path_env[i])
+	{
+		path_join = ft_strjoin(path_env[i], "/");
+		cmd->path = ft_strjoin (path_join, cmd->cmd[0]);
+		free(path_join);
+		if (access(cmd->path, F_OK) == 0)
+			return (cmd->path);
+		free(cmd->path);
+		cmd->path = NULL;
+		i++;
+	}
+	return (NULL);
+}
+
+/*Looks through the environment to find an executable path for the command sent
+as an argument.*/
+char	*find_path(t_command **cmd, char **env)
+{
+	int		i;
+	char	**path_env;
+
+	i = 0;
+	while (env[i])
+	{
+		if (ft_strncmp("PATH=", env[i], 5) == 0)
+			break ;
+		i++;
+		if (env[i] == NULL || env[i + 1] == NULL)
+			return (ENV_ERROR);
+	}
+	path_env = (char **)ft_split(env[i] + 5, ':');
+	i = 0;
+	(*cmd)->path = access_path(*cmd, path_env);
+	return ((*cmd)->path);
+}
+
+t_command	*find_cmd(t_command **cmd)
+{
+	(*cmd)->path = find_path(cmd, use_data()->new_env);
+	if ((*cmd)->path == NULL
+		// || ft_strncmp(ENV_ERROR, cmd->path, 43) == 0
+		)
+	{
+		printf("COULD NOT FIND PATH\n");
+	}
+	return (*cmd);
+}
+
 int	count_commands(t_command *cmd)
 {
 	int	i;
@@ -34,8 +88,6 @@ void	exec(t_command *cmd)
 	int	nb_cmds;
 
 	nb_cmds = count_commands(cmd);
-	// dup2(cmd->infile, STDIN_FILENO);
-	// close (cmd->infile);
 	while (cmd)
 	{
 		printf("%s\n", cmd->cmd[0]);
@@ -47,7 +99,10 @@ void	exec(t_command *cmd)
 		else if (cmd->builtin_flag == YES)
 			printf("is a builtin)\n");
 		if (nb_cmds == 1)
-			printf("execute the command directly\n");
+		{
+			find_cmd(&cmd);
+			printf("%s\n", cmd->path);
+		}
 		else
 			pipex(cmd->cmd, nb_cmds);
 		if (cmd->next)
